fae7c4558dbfe2af727ccf575136491ee3873631
Suman <sumandeep4300@gmail.com>
2021-12-07 21:32:45 +0530

    added symbol-patterns

---
+++ Symbol Patterns/symbolpattern6.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n = 5;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int k = n; k > i; k--) {
+            cout << " ";
+        }
+        for (int j = 1; j <= i; j++) {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
