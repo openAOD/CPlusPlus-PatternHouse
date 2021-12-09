f2d7f1696e1ea7e1e958d60514d4499a64b03213
Vikas Yadav <86154844+VikasYadav-1@users.noreply.github.com>
2021-12-09 17:02:31 +0530

    Create alphabeticpattern38.cpp

---
+++ Alphabetic Patterns/alphabeticpattern38.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+//#include<iostream>
+using namespace std;
+int main(){
+    for(int i=0;i<5;i++){   
+        int k=4-i;
+        while (k--)         //for spaces before first character
+        {
+            cout<<"  ";
+        }
+        
+        for (int j = 0; j < 2*i+1; j++)
+        {
+            char a=65+j;
+            cout<<a<<" ";
+        }
+        cout<<endl;
+    }
+ return 0;
+}
