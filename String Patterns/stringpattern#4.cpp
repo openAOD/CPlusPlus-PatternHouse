d44ee4e7d05e096770be619fd4484bbc5a1bb7d0
Srishti Ahuja <57188326+Srishti-Ahuja@users.noreply.github.com>
2021-12-13 11:48:58 +0530

    Added String Pattern 4

---
+++ String Patterns/stringpattern#4.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int n=5;
+    for(int i=0; i<n; i++){
+        char ch='A';
+        for(int j=0; j<(n-i); j++){
+            cout<<ch<<" ";
+            ch++;
+        }
+        cout<<endl;
+    }
+}
