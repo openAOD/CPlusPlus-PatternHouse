62b7111a1d8ed069a68d0e89fab6c629c6fe1a45
Srishti Ahuja <57188326+Srishti-Ahuja@users.noreply.github.com>
2021-12-13 12:04:25 +0530

    Added String Pattern 9

---
+++ String Patterns/stringpattern#9.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int n=5;
+    char ch='A';
+    for(int i=0; i<n; i++){
+        for(int j=0; j<i+1; j++){
+            cout<<ch<<" ";
+            ch++;
+        }
+        cout<<endl;
+    }
+}
