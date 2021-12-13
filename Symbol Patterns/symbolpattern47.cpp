d1b7ece7946a66d553c705efa215ee1dc20e0175
Bobby Kumar <kashyapashish0002@gmail.com>
2021-12-13 19:04:04 +0530

    Added code for symbol pattern 47

---
+++ Symbol Patterns/symbolpattern47.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+
+void printPattern (int);
+
+
+int main()
+{
+    system("cls");
+
+    int totalLines;
+
+    cout << "How many lines to be printed? --> ";
+    cin >> totalLines;
+
+    cout << "\nYour pattern looks like this:\n" << endl;
+    printPattern(totalLines);
+
+    cout<<"\n";
+    return 0;
+}
+
+void printPattern (int totalLines) {
+
+    for (int i = totalLines; i >= 1; i--) {;
+
+        for (int j = 1; j <= i; j++) {
+
+            if (j & 1) {
+
+                cout << j << " ";
+            } else {
+
+                cout << "* ";
+            }
+        }
+
+        cout << endl;
+    }
+}
