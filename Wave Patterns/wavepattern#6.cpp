b48934a74b5a828871d1f7e0508a524120d0ac4e
Srishti Ahuja <57188326+Srishti-Ahuja@users.noreply.github.com>
2021-12-14 15:42:42 +0530

    Added Wave Pattern 1,3,4,5,6,7,8

---
+++ Wave Patterns/wavepattern#6.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int n=4;
+    int height=n-1, length=n, pc=n-1;
+
+    for(int i=0; i<height+1; i++){
+        for(int j=0; j<(2*height*length) +1; j++){
+            if ((j % (height * 2) == pc) || (j % (height * 2) == height + i)){
+                char ch='Z'-j;
+                cout<<ch<<" ";
+            }
+            else
+                cout<<"  ";
+        }
+        pc -= 1;
+        cout<<endl;
+    }
+}
