ffa217d65b9a132311044a46aa6df1eba53cbe4f
anshyyy <anshuman9998@gmail.com>
2021-12-16 20:08:22 +0530

    Solved issue #332

---
+++ Alphabetic Patterns/alphabeticpattern172.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int i, j;
+    int height = 5;
+    int width = 2*height-1;
+    width--;
+    for (i = 0; i < height; i++) {
+        for (j = 0; j < width; j++) {
+            if ((i == 0 || i == height - 1)
+                && (j == 0 || j == width - 1))
+                cout <<" ";
+            else if (j == 0)
+                cout <<"G";
+            else if (i == 0 && j <= height)
+                cout <<"G";
+            else if (i == height / 2
+                    && j > height / 2)
+                cout <<"G";
+            else if (i > height / 2
+                    && j == width - 1)
+                cout <<"G";
+            else if (i == height - 1
+                    && j < width)
+                cout <<"G";
+            else
+                cout <<" ";
+        }
+        cout <<"\n";
+    }
+    return 0;
+}
