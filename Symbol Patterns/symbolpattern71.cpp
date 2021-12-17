bd7b4113e0cc975c9c9228d66d72445773203fca
Suman <sumandeep4300@gmail.com>
2021-12-17 17:34:55 +0530

    added symbol patterns

---
+++ Symbol Patterns/symbolpattern71.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n = 5;
+	
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= n; j++) {
+			if (j == i || j == (n + 1 - i))
+				cout << "*";
+			else
+				cout << " ";
+		}
+		cout << endl;
+	}
+	return 0;
+}
