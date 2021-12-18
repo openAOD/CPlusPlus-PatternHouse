f9041182f584ae6ec6241bc5e93d8917fd4f538a
Bobby Kumar <kashyapashish0002@gmail.com>
2021-12-18 20:06:06 +0530

    Added code for pyramid pattern from 11 to 20.

---
+++ Pyramid Patterns/pyramidpattern20.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+using namespace std;
+
+void printPattern (int);
+void printWhitespaces (int);
+
+int main () {
+
+    int totalLines;
+
+    cout << "How many lines to be printed? -- ";
+    fflush(stdout);
+    cin >> totalLines;
+
+    cout << "\nYour pattern looks like this:\n";
+    cout << "\n";
+
+    printPattern (totalLines);
+
+    cout << "\n";
+    return 0;
+}
+
+void printPattern (int totalLines) {
+
+    int whiteSpace, printStar, counter = 1;
+
+    for (int i = 1; i <= totalLines; i++) {
+
+        whiteSpace = (totalLines - i);
+        printWhitespaces (whiteSpace);
+
+        printStar = ((2 * i) - 1);
+
+        for (int j = 1; j <= printStar; j++) {
+
+            cout.width(2);
+            cout << counter++ << " ";
+        }
+
+        cout << endl;
+    }
+}
+
+void printWhitespaces (int total) {
+
+    for (int i = 1; i <= total; i++) {
+
+        cout << "   ";
+    }
+}
