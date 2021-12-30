25ee84b2bf3065c1c82c00d8df35b88acdd83bc6
Arkaprava Nath <natharkaprava@gmail.com>
2021-12-30 08:46:00 +0530

    10 patterns added

---
+++ Alphabetic Patterns/alphabeticpattern138.cpp
@@ -0,0 +1,28 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    for (int i = 1; i <= 5; i++)
+    {
+        for (int j = 1; j <= (5 - i); j++)
+        {
+            cout << " ";
+        }
+        for (int j = 1; j <= i; j++)
+        {
+            cout << (char)(j + 64);
+        }
+        for (int j = 1; j <= i; j++)
+        {
+            cout << j;
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
