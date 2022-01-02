5b013c31106694be25c4906522a60d9d05b85c4b
Mahak008 <88129955+Mahak008@users.noreply.github.com>
2022-01-02 14:05:59 +0530

    Create numericpattern225.cpp

---
+++ Numeric Patterns/numericpattern225.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin>>n; // No. of lines to be printed
+    
+    for(int row = 0; row <= 5; row++) {
+        for(int col = 1; col <= n - row + 1; col++) {
+            if(col == n - row + 1) {
+                cout<<(n - row)<<" ";
+            }
+            else {
+                cout<<"* ";
+            }
+        }
+        cout<<endl;
+    }
+}
+
+// Sample Input :- 5
+// * * * * * 5
+// * * * * 4
+// * * * 3
+// * * 2
+// * 1
+// 0
+
+
