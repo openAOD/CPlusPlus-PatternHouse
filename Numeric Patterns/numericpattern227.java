3a9de8e227ce36dd7a5d427a8e94bfb815f5d437
Mahak008 <88129955+Mahak008@users.noreply.github.com>
2022-01-02 14:15:26 +0530

    Create numericpattern227.java

---
+++ Numeric Patterns/numericpattern227.java
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin>>n; // No. of lines to be printed
+    
+    for(int row = 1; row <= 5; row++) {
+        for(int col = 1; col <= n; col++) {
+            if(col == row || n - col + 1 == row) {
+                cout<<n - row + 1<<" ";
+            }
+            else {
+                cout<<"  ";
+            }
+        }
+        cout<<endl;
+    }
+}
+
+// Sample Input :- 5
+// Output :-
+// 5       5
+//   4   4
+//     3
+//   2   2
+// 1       1
+
+
