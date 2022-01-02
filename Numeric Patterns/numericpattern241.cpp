728f1299a0c73e61e69b684972896df3f4f0d90f
Mahak008 <88129955+Mahak008@users.noreply.github.com>
2022-01-02 14:57:37 +0530

    Create numericpattern241.cpp

---
+++ Numeric Patterns/numericpattern241.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin>>n;
+    
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= i; j++) {
+            if (i==n){
+                cout<<n;
+            }
+            else if(j==1 || j==i){
+              cout<<i;
+            } 
+            else {
+                cout<<" ";
+            }
+        }
+        cout<<endl;
+    }
+}
+
+// Sample Input :- 5
+// Output :- 
+// 1
+// 22
+// 3 3
+// 4  4
+// 55555
