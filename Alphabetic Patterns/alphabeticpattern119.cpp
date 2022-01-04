cb352fb6a65189ed0d87cf65590392121fe68f27
Mahak008 <88129955+Mahak008@users.noreply.github.com>
2022-01-04 14:04:42 +0530

    Update alphabeticpattern119.cpp

--- Alphabetic Patterns/alphabeticpattern119.cpp
+++ Alphabetic Patterns/alphabeticpattern119.cpp
@@ -3,8 +3,9 @@ using namespace std;
 
 int main() {
     int n; // Input no. of lines to be printed
-    int y; // Input no. of timed pattern to be printed
+    int y; // Input no. of times pattern to be printed
     cin>>n>>y;
+    
     for(int k = 0; k < y; k++) {
         for(int i = 1;i <= n; i++) {
             for(int j = 1;j <= i; j++) {
