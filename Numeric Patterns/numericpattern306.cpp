af28cd5b83863da7020e70851ee08953cb671c26
Mahak008 <88129955+Mahak008@users.noreply.github.com>
2022-01-07 14:28:12 +0530

    Update numericpattern306.cpp

--- Numeric Patterns/numericpattern306.cpp
+++ Numeric Patterns/numericpattern306.cpp
@@ -6,7 +6,7 @@ int main() {
     cin>>height;
     
     for(int i = 1;i <= height; i++) {
-        for(int j=1;j<=5-i;j++) {
+        for(int j=1;j<= height-i;j++) {
             cout<<"   ";
         }
         for(int j=1;j<=i;j++) {
